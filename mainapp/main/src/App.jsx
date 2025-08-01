68f645c93b0d7dbc834787e594baf14d55967cca
Vivek Thakur <vivekthakurcse20509@gmail.com>
2025-08-01 10:39:23 +0000

    commit

---
+++ mainapp/main/src/App.jsx
@@ -0,0 +1,13 @@
+import MenuBar from "./component/menubar/menubar";
+const App = () => {
+  return (
+    <>
+      <div className="root">
+        <MenuBar />
+        <h1>My App</h1>
+      </div>
+    </>
+  );
+};
+
+export default App;
---
+++ mainapp/main/src/component/menubar/menubar.jsx
@@ -0,0 +1,13 @@
+const MenuBar = () => {
+  return (
+    <div className="menu-bar">
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+    </div>
+  );
+};
+
+export default MenuBar;
